cf864652631239ff4e38944a0cbc93289b399382
root <root@wongkar-cr-a7a50bab.cs14bcloudraya.internal>
2022-08-27 03:32:38 +0000

    aa

---
+++ wongkar_selling/wongkar_selling/report/profit_and_loss_indonesia/profit_and_loss_indonesia.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, w and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Profit and Loss Indonesia"] = {
+	"filters": [
+
+	]
+};
