d1643d33e12e494bd2ac8ce8d4cabfb57a4df4a5
bobzz-zone <bobzz.zone@gmail.com>
2022-09-02 00:13:27 +0700

    pnl

--- wongkar_selling/wongkar_selling/report/profit_and_loss_indonesia_detail/profit_and_loss_indonesia_detail.js
+++ wongkar_selling/wongkar_selling/report/profit_and_loss_indonesia_detail/profit_and_loss_indonesia_detail.js
@@ -2,8 +2,26 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Profit and Loss Indonesia Detail"] = {
-	"filters": [
+frappe.require("assets/erpnext/js/financial_statements.js", function() {
+	frappe.query_reports["Profit and Loss Indonesia Detail"] = $.extend({},
+		erpnext.financial_statements);
 
-	]
-};
+	erpnext.utils.add_dimensions('Profit and Loss Indonesia Detail', 10);
+
+	frappe.query_reports["Profit and Loss Indonesia Detail"]["filters"].push(
+		{
+			"fieldname": "project",
+			"label": __("Project"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Project', txt);
+			}
+		},
+		{
+			"fieldname": "include_default_book_entries",
+			"label": __("Include Default Book Entries"),
+			"fieldtype": "Check",
+			"default": 1
+		}
+	);
+});
