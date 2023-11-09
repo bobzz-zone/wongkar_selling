48b7b6f77f2c46f76beef9f2f0e4bdd76530e3d0
root <root@ip-172-26-11-113.ap-southeast-1.compute.internal>
2023-11-09 02:37:25 +0000

    udpate js penerimaan dp 09-11-2023

--- wongkar_selling/wongkar_selling/doctype/penerimaan_dp/penerimaan_dp.js
+++ wongkar_selling/wongkar_selling/doctype/penerimaan_dp/penerimaan_dp.js
@@ -164,8 +164,8 @@ frappe.ui.form.on('Penerimaan DP', {
 		if(cur_frm.doc.dp_ke_2){
 			frappe.msgprint("Lakukan Perhitungan Secara Manual !!!")
 		}else{
-			cur_frm.set_value("nominal_diskon",0)
-			cur_frm.refresh_fields("nominal_diskon")
+			// cur_frm.set_value("nominal_diskon",0)
+			// cur_frm.refresh_fields("nominal_diskon")
 		}
 	},
 	cara_bayar(frm){
